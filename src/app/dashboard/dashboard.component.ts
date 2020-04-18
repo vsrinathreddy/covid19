fc7ba39a4923b85cf10f95b8f981a819c3b5c76d
V Srinath Reddy <srinath.vanam@hotmail.com>
2020-04-18 17:01:10 +0530

    dashbord footer

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -14,6 +14,7 @@ import {MatTableDataSource} from '@angular/material/table';
 export class DashboardComponent implements OnInit {
   displayedColumns = ['stateName', 'caseCount', 'cured', 'death'];
   dataSource: MatTableDataSource<Dailycases>;
+  dailyCases: Dailycases[] =[];
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private formBuilder: FormBuilder, private router: Router, private dailyCaseService: DailycasesService) { }
@@ -22,8 +23,21 @@ export class DashboardComponent implements OnInit {
     this.dailyCaseService.getDailyCases().subscribe((data: any[])=>{
       this.dataSource = new MatTableDataSource(data);
       this.dataSource.sort = this.sort;
+      this.dailyCases=data;
     })
 
+  }
+
+  getTotalCases(type:number) {
+    if(type==1){
+      return this.dailyCases.map(t => t.caseCount).reduce((acc, value) => acc + value, 0);
+    }
+    else if(type==2){
+      return this.dailyCases.map(t => t.cured).reduce((acc, value) => acc + value, 0);
+    }
+    else {
+      return this.dailyCases.map(t => t.death).reduce((acc, value) => acc + value, 0);
+    }
   }
 
 }
