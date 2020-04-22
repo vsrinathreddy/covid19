36e5acfb2234cb73ad2fae74192af58046f7c4d0
V Srinath Reddy <srinath.vanam@hotmail.com>
2020-04-22 21:11:05 +0530

    title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {Router} from "@angular/router"
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Covid19India';
+  title = 'Covid19 India';
   constructor(private router: Router) {
     //this.router.navigate(['/dashboard'])
    }
