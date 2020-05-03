51f16c92d35df40cdc90239767f785cf4decd9b3
V Srinath Reddy <srinath.vanam@hotmail.com>
2020-05-03 21:52:53 +0530

    credits update

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectorRef,Component, OnInit,ViewChild } from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
-import {Router} from "@angular/router"
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Router } from "@angular/router"
 import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
@@ -9,36 +9,44 @@ import { MatSidenav } from '@angular/material/sidenav';
   styleUrls: ['./site-header.component.css']
 })
 export class SiteHeaderComponent implements OnInit {
-
+  credits: string = "Developed by Srinath";
+  creditsmobile: string = " by Srinath";
   mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,private router: Router) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
-  @ViewChild('sidenav', {static: true})
+  @ViewChild('sidenav', { static: true })
   public sidenav: MatSidenav;
   ngOnInit(): void {
-
+    if (this.mobileQuery.matches) {
+      this.credits = "";
+      this.creditsmobile = " by Srinath";
+    }
+    else{
+      this.credits = "Developed by Srinath";
+      this.creditsmobile = "";
+    }
   }
-  goToURL(path:string){
-    if(path=='dashboard'){
+  goToURL(path: string) {
+    if (path == 'dashboard') {
       this.router.navigate(['/dashboard'])
     }
-    else if(path=='login'){
+    else if (path == 'login') {
       this.router.navigate(['/login'])
     }
-    else if(path=='cases'){
+    else if (path == 'cases') {
       this.router.navigate(['/cases'])
     }
-    else if(path=='charts'){
+    else if (path == 'charts') {
       this.router.navigate(['/charts'])
     }
 
-      this.sidenav.close();
+    this.sidenav.close();
 
   }
   ngOnDestroy(): void {
